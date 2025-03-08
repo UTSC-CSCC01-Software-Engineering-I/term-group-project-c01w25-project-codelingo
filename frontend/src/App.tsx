4a77a446a02aa47d507be834fa2b3e9c0075d622
Christian Rogers <135765678+christianGRogers@users.noreply.github.com>
2025-03-07 21:06:21 -0500

    Revert "Feature/ap 27/add lessons to admin dashboard"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,6 @@ import About from "./app/home/about";
 import Contact from "./app/home/contact";
 import DiscussionsPage from "./app/discussion/discussionPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from "./app/registration/registrationPage";
-import Login from "./app/login/loginPage";
 
 function App() {
   return (
@@ -18,8 +16,6 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/discussions" element={<DiscussionsPage />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
